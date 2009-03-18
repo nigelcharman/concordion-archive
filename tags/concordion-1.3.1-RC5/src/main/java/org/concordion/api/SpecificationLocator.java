0909c8d79096b0930226445238415ced03613d82
jose.m.beas <jose.m.beas@430547ba-244a-0410-acf3-eb946b82c357>
2009-03-18 01:28:13 +0000

    Remove this tag in order to repeat the process.

--- tags/concordion-1.3.1-RC5/src/main/java/org/concordion/api/SpecificationLocator.java
+++
@@ -1,6 +0,0 @@
-package org.concordion.api;
-
-public interface SpecificationLocator {
-
-    Resource locateSpecification(Object fixture);
-}
--- tags/concordion-1.3.1-RC5/src/test/java/spec/concordion/command/verifyRows/results/SurplusRowsTest.java
+++
@@ -1,9 +0,0 @@
-package spec.concordion.command.verifyRows.results;
-
-public class SurplusRowsTest extends MissingRowsTest {
-
-    public void addPerson(String firstName, String lastName) {
-        super.addPerson(firstName, lastName, 1973);
-    }
-
-}
