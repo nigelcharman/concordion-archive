e086a1ecfb0ef217c36de305a360f9c53fdea28c
jose.m.beas <jose.m.beas@430547ba-244a-0410-acf3-eb946b82c357>
2009-03-24 15:13:07 +0000

    Remove tag for 1.3.1-RC5 in order to repeat the release process but with deployment in a remote repo in order to sync with Maven Central Repo.

--- tags/concordion-1.3.1-RC5/src/main/java/org/concordion/api/Runner.java
+++
@@ -1,6 +0,0 @@
-package org.concordion.api;
-
-public interface Runner {
-	
-	RunnerResult execute(Resource resource, String href) throws Exception;
-}
--- tags/concordion-1.3.1-RC5/src/test/java/spec/concordion/results/assertEquals/success/HasClassAttributeTest.java
+++
@@ -1,6 +0,0 @@
-package spec.concordion.results.assertEquals.success;
-
-
-public class HasClassAttributeTest extends SuccessTest {
-    
-}
