b37f8fee76c1464b8a236d54ee8475c0bae36240
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2009-12-19 20:57:22 +0000

    Maven problems - deleting and retrying.

--- tags/concordion-1.3.2/src/test/java/spec/concordion/results/assertEquals/failure/AnchorsTest.java
+++
@@ -1,8 +0,0 @@
-package spec.concordion.results.assertEquals.failure;
-
-import org.concordion.integration.junit3.ConcordionTestCase;
-
-public class AnchorsTest extends ConcordionTestCase {
-    // TODO
-
-}
