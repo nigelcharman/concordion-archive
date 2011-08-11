67ab3e1802662387369127b25235d3336d920ce9
nigel.charman.nz@gmail.com <nigel.charman.nz@gmail.com@430547ba-244a-0410-acf3-eb946b82c357>
2011-08-11 10:31:44 +0000

    Refactored extension loader code

--- concordion/src/main/java/org/concordion/internal/extension/ExtensionLoader.java
+++
@@ -1,9 +0,0 @@
-package org.concordion.internal.extension;
-
-import org.concordion.internal.ConcordionBuilder;
-
-public interface ExtensionLoader {
-
-    void addExtensions(Object fixture, ConcordionBuilder concordionBuilder);
-
-}
