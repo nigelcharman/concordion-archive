3c621fd5f680b939b1bcac6e043a99daf1ac4304
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2011-10-31 20:15:58 +0000

    Deleting old branch

--- branches/peterson3/concordion/src/test/java/spec/concordion/annotation/AnnotationTest.java
+++
@@ -1,7 +0,0 @@
-package spec.concordion.annotation;
-
-import org.concordion.integration.junit3.ConcordionTestCase;
-
-public class AnnotationTest extends ConcordionTestCase {
-
-}
--- branches/peterson3/concordion/src/test/java/spec/concordion/integration/junit3/Junit3Test.java
+++
@@ -1,8 +0,0 @@
-package spec.concordion.integration.junit3;
-
-import org.concordion.integration.junit3.ConcordionTestCase;
-
-public class Junit3Test extends ConcordionTestCase {
-
-    
-}
