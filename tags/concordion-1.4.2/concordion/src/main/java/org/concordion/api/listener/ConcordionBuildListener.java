aab9c6b973998ba49ee51f44aa57d1f9db1fd8de
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2011-10-31 20:01:31 +0000

    Deleting unneeded tag

--- tags/concordion-1.4.2/concordion/src/main/java/org/concordion/api/listener/ConcordionBuildListener.java
+++
@@ -1,7 +0,0 @@
-package org.concordion.api.listener;
-
-import java.util.EventListener;
-
-public interface ConcordionBuildListener extends EventListener {
-    public void concordionBuilt(ConcordionBuildEvent event);
-}
