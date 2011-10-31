43ada7de16229f6959b98c47295ff7e8883d4058
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2011-10-31 00:19:33 +0000

    Switched to a simulated (in-memory) file manager for the Java compiler to avoid platform-dependent classpath issues.

--- trunk/concordion/src/test/java/test/concordion/JavaSourceCompiler.java
+++ trunk/concordion/src/test/java/test/concordion/JavaSourceCompiler.java
@@ -1,55 +1,20 @@
 package test.concordion;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Arrays;
-
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.ToolProvider;
+import test.concordion.compiler.CompilationFailedException;
+import test.concordion.compiler.Source;
 
 public class JavaSourceCompiler {
 
-    private static final String TARGET_DIR = "target/genClasses";
-    private static final String CLASS_PATH = System.getProperty("java.class.path") + ":" + TARGET_DIR;
-
+    private test.concordion.compiler.JavaCompiler compiler = new test.concordion.compiler.JavaCompiler();
+    
     public Class<?> compile(String className, String sourceCode) throws Exception {
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        
-        StandardJavaFileManager fileManager;
-        try {
-            fileManager = compiler.getStandardFileManager(null, null, null);
-            new File(TARGET_DIR).mkdirs();
-        } catch (NullPointerException e) {
-            throw new IllegalStateException("Test requires the system Java compiler. " +
-            		"Check that your classpath contains tools.jar, or run the tests using a Java Development Kit (JDK), rather than a Java Runtime Environment (JRE). " +
-            		"The Java version must be 1.6.0 or later.");
-        }
         
         try {
-            JavaSourceFileObject source = new JavaSourceFileObject(className, sourceCode);
-            Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(new JavaSourceFileObject[] { source });
-    
-            Iterable<String> options = Arrays.asList(new String[] { "-classpath", CLASS_PATH, "-d", TARGET_DIR});
-            Boolean compiledOK = compiler.getTask(null, fileManager, null, options, null, compilationUnits).call();
-            if (!compiledOK) {
-                throw new RuntimeException("Compilation failure");
-            }
-    
-            ClassLoader urlClassLoader = new URLClassLoader(new URL[] { new File(TARGET_DIR).toURI().toURL() });
-            Class<?> compiledClass = urlClassLoader.loadClass(className);
-    
-            return compiledClass;
-
-        } finally {
-            try {
-                fileManager.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            compiler.compile(new Source(sourceCode, className + ".java"));
+        } catch (CompilationFailedException e) {
+            e.printDiagnosticsTo(System.out);
         }
+        
+        return compiler.forName(className);
     }
 }
