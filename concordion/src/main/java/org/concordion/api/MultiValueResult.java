c5dc278c9be230c308827b7848d665c64402553e
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2011-12-09 09:12:07 +0000

    Removed unnecessary "this"

--- concordion/src/main/java/org/concordion/api/MultiValueResult.java
+++ concordion/src/main/java/org/concordion/api/MultiValueResult.java
@@ -7,7 +7,7 @@ public class MultiValueResult extends HashMap<String, Object> {
     private static final long serialVersionUID = 1845462548653884738L;
     
     public MultiValueResult with(String key, Object value) {
-        this.put(key, value);
+        put(key, value);
         return this;
     }
     
