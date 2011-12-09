2de99ce5690d827e50907fa613a6b77f68912db5
peterson3 <peterson3@430547ba-244a-0410-acf3-eb946b82c357>
2011-12-09 08:41:41 +0000

    Removed unnecessary constructor

--- concordion/src/main/java/org/concordion/api/MultiValueResult.java
+++ concordion/src/main/java/org/concordion/api/MultiValueResult.java
@@ -6,9 +6,6 @@ public class MultiValueResult extends HashMap<String, Object> {
 
     private static final long serialVersionUID = 1845462548653884738L;
     
-    public MultiValueResult() {
-    }
-    
     public MultiValueResult with(String key, Object value) {
         this.put(key, value);
         return this;
