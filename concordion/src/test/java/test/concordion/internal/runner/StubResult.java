063a89d1bbab8dff5b37f66920a81504e4ec8ca5
peterson3@gmail.com <peterson3@gmail.com@430547ba-244a-0410-acf3-eb946b82c357>
2013-01-26 17:35:40 +0000

    Added serialVersionUID to stop an Eclipse warning

--- concordion/src/test/java/test/concordion/internal/runner/StubResult.java
+++ concordion/src/test/java/test/concordion/internal/runner/StubResult.java
@@ -2,7 +2,6 @@ package test.concordion.internal.runner;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -12,22 +11,23 @@ import org.junit.runner.notification.Failure;
  */
 public class StubResult extends Result {
 
+    private static final long serialVersionUID = 6102153773248794468L;
     public static final StubResult SUCCESS = new StubResult();
     public static final StubResult FAILURE = new StubResult().withFailure(new AssertionError("Dummy test failure"));
 
     private List<Failure> failures = new ArrayList<Failure>();
     private int ignoreCount;
-    
+
     public StubResult withFailure(Throwable error) {
         failures.add(new Failure(Description.EMPTY, error));
         return this;
     }
-    
+
     public StubResult withIgnoreCount(int ignoreCount) {
         this.ignoreCount = ignoreCount;
         return this;
     }
-    
+
     @Override
     public boolean wasSuccessful() {
         return failures.size() == 0;
